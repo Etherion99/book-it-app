6445089bed55a0ec1cb9de4af0f0d482779083e6
Etherion99 <37667366+Etherion99@users.noreply.github.com>
2021-10-21 13:48:34 -0500

    login styles

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  form = new FormGroup({
+    email: new FormControl('juansttt99@gmail.com'),
+    password: new FormControl('12345'),
+  });
 
-  constructor() { }
+  constructor(
+    private server: ApiService
+  ) { }
 
   ngOnInit(): void {
   }
